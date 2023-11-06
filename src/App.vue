9511033e6444fc3ab66eeaddd4b110eaa8e580dd
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-06 16:46:35 +0100

    ADD in page PageFooter

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script>
   import PageHeader from './components/PageHeader.vue';
+  import PageFooter from './components/PageFooter.vue';
 
   export default {
     components: {
       PageHeader,
+      PageFooter
     }
   }
 </script>
 
 <template>
   <PageHeader/>
+  <PageFooter/>
 </template>
 
 <style lang="scss">
