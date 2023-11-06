0b9f32b51939e28e4293277bd618ff2dc8a6b6f1
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-06 17:11:27 +0100

    Import PageMain to page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script>
   import PageHeader from './components/PageHeader.vue';
   import PageFooter from './components/PageFooter.vue';
+  import PageMain from './components/PageMain.vue';
 
   export default {
     components: {
       PageHeader,
-      PageFooter
+      PageMain,
+      PageFooter,
     }
   }
 </script>
 
 <template>
   <PageHeader/>
+  <PageMain/>
   <PageFooter/>
+ 
 </template>
 
 <style lang="scss">
