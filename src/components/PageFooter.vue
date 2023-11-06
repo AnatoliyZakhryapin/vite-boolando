a5785be6d4476336abf6be91cc5cf19f93e129ef
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-06 16:45:58 +0100

    ADD Html PageFooter

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,61 @@
+<script></script>
+
+<template>
+     <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="widget">
+                        <h3 class="widget__title">Boolando s.r.l.</h3>
+                        <ul class="widget__menu">
+                            <li class="widget__menu__item">
+                                <a href="">Informazioni legali</a>
+                            </li>
+                            <li class="widget__menu__item">
+                                <a href="">Informativa sulla privacy</a>
+                            </li>
+                            <li class="widget__menu__item">
+                                <a href="">Diritto di recesso</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-6 flex justify-content-flex-end">
+                    <div class="widget">
+                        <h3 class="widget__title">Trovaci anche su</h3>
+                        <ul class="widget__menu">
+                            <li class="widget__menu__item">
+                                <a href="">
+                                    <img src="/img/twitter.png" alt="">
+                                </a>
+                            </li>
+                            <li class="widget__menu__item">
+                                <a href="">
+                                    <img src="/img/instagram.png" alt="">
+                                </a>
+                            </li>
+                            <li class="widget__menu__item">
+                                <a href="">
+                                    <img src="/img/facebook.png" alt="">
+                                </a>
+                            </li>
+                            <li class="widget__menu__item">
+                                <a href="">
+                                    <img src="/img/tiktok.png" alt="">
+                                </a>
+                            </li>
+                            <li class="widget__menu__item">
+                                <a href="">
+                                    <img src="/img/youtube.png" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped></style>
