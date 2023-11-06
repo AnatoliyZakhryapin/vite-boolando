b22701431f9d641e59fee908f76fe7a72fd15970
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-06 16:04:34 +0100

    ADD HTML to PageHeader.vue

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,31 @@
+<script></script>
+
+<template>
+    <header class="header-nav">
+        <nav class="container">
+            <div class="row">
+                <div class="col">
+                    <ul class="header-nav__gender-menu">
+                        <li class="gender-menu__item"><a href="">Donna</a></li>
+                        <li class="gender-menu__item"><a href="">Uomo</a></li>
+                        <li class="gender-menu__item"><a href="">Bambini</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <div class="logo-content">
+                        <img class="logo" src="/img/boolean-logo.png" alt="">
+                    </div>
+                </div>
+                <div class="col">
+                    <ul class="header-nav__icon-menu">
+                        <li class="icon-menu__item"><a href=""><img src="/img/persona.png" alt=""></a></li>
+                        <li class="icon-menu__item"><a href=""><img src="/img/cuore.png" alt=""></a></li>
+                        <li class="icon-menu__item"><a href=""><img src="/img/borsa.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>   
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped></style>
